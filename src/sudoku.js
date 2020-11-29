5a1e19f39a4f8beec708c4f16d7202536026ad02
guntankoba <kbys.rx.75.0917@gmail.com>
2020-11-29 15:28:46 +0900

    add transferMatrix method

--- src/sudoku.js
+++ src/sudoku.js
@@ -15,16 +15,49 @@ console.log('default');
 printBoard(board);
 console.log(board[0]);
 var test_json_1 = __importDefault(require("./data/test.json"));
-var pazzle = importPazzle('data', 1);
-console.log(pazzle);
 //method
 function printBoard(board) {
     console.log(board);
 }
 function importPazzle(key, id) {
+    // get nubmerlist from jsonfile
     console.log(key, id);
     console.log(test_json_1.default.data[id]);
     var data = test_json_1.default.data[id];
     var pazzleData = data.split('').map(function (x) { return parseInt(x); });
     return pazzleData;
 }
+var Board = /** @class */ (function () {
+    function Board(key, id) {
+        this.key = key;
+        this.id = id;
+        this.data = this.importData(this.key, this.id);
+    }
+    Board.prototype.transferMatrix = function (array) {
+        var dataMatrix = new Array();
+        var s = 0;
+        for (var i = 0; i < 9; i++) {
+            dataMatrix[i] = new Array();
+            for (var j = 0; j < 9; j++) {
+                dataMatrix[i][j] = this.data[s];
+                s++;
+            }
+        }
+        return dataMatrix;
+    };
+    Board.prototype.printData = function () {
+        var dataMatrix = this.transferMatrix(this.data);
+        console.log(dataMatrix);
+    };
+    Board.prototype.importData = function (key, id) {
+        // get nubmerlist from jsonfile
+        console.log(key, id);
+        console.log(test_json_1.default.data[id]);
+        var data = test_json_1.default.data[id];
+        var pazzleData = data.split('').map(function (x) { return parseInt(x); });
+        return pazzleData;
+    };
+    return Board;
+}());
+var b = new Board('data', 1);
+b.printData();
--- src/sudoku.ts
+++ src/sudoku.ts
@@ -13,9 +13,6 @@ console.log(board[0])
 
 import testData from './data/test.json'
 
-let pazzle: number[] = importPazzle('data', 1)
-
-console.log(pazzle)
 
 //method
 function printBoard(board: number[][]) {
@@ -30,3 +27,43 @@ function importPazzle(key: string , id: number){
     const pazzleData: number[] = data.split('').map(x => parseInt(x))
     return pazzleData
 }
+
+class Board {
+    //propaty
+    data: number[]
+
+    constructor(private key: string, private id: number) {
+        this.data = this.importData(this.key, this.id) 
+    }
+
+    private transferMatrix(array: number[]): number[][] {
+        let dataMatrix = new Array();
+        let s = 0;
+        for (let i = 0; i < 9; i++) {
+            dataMatrix[i] = new Array();
+            for (let j=0; j < 9; j++) {
+                dataMatrix[i][j] = this.data[s];
+                s ++;
+            }
+        }
+        return dataMatrix
+    }
+
+    printData() {
+        const dataMatrix = this.transferMatrix(this.data)
+        console.log(dataMatrix)
+        
+    }
+
+    private importData(key: string , id: number){
+        // get nubmerlist from jsonfile
+        console.log(key, id)
+        console.log(testData.data[id])
+        const data: string = testData.data[id]
+        const pazzleData: number[] = data.split('').map(x => parseInt(x))
+        return pazzleData
+    }
+}
+
+const b = new Board('data', 1)
+b.printData()
