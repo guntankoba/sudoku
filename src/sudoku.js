db32efaa2c6536b6e33b89f2f982c85030a2dc3e
guntankoba <kbys.rx.75.0917@gmail.com>
2020-11-29 17:21:04 +0900

    add function
    add getRow, getColumn, getSquare

--- src/sudoku.js
+++ src/sudoku.js
@@ -3,35 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var board = new Array();
-printBoard(board);
-for (var i = 0; i < 9; i++) {
-    board[i] = new Array();
-    for (var j = 0; j < 9; j++) {
-        board[i][j] = i * 9 + (j + 1);
-    }
-}
-console.log('default');
-printBoard(board);
-console.log(board[0]);
 var test_json_1 = __importDefault(require("./data/test.json"));
-//method
-function printBoard(board) {
-    console.log(board);
-}
-function importPazzle(key, id) {
-    // get nubmerlist from jsonfile
-    console.log(key, id);
-    console.log(test_json_1.default.data[id]);
-    var data = test_json_1.default.data[id];
-    var pazzleData = data.split('').map(function (x) { return parseInt(x); });
-    return pazzleData;
-}
 var Board = /** @class */ (function () {
     function Board(key, id) {
         this.key = key;
         this.id = id;
+        this.SQUARE_POSITION = [];
         this.data = this.importData(this.key, this.id);
+        this.dataMatrix = this.transferMatrix(this.data);
     }
     Board.prototype.transferMatrix = function (array) {
         var dataMatrix = new Array();
@@ -45,10 +24,6 @@ var Board = /** @class */ (function () {
         }
         return dataMatrix;
     };
-    Board.prototype.printData = function () {
-        var dataMatrix = this.transferMatrix(this.data);
-        console.log(dataMatrix);
-    };
     Board.prototype.importData = function (key, id) {
         // get nubmerlist from jsonfile
         console.log(key, id);
@@ -57,7 +32,46 @@ var Board = /** @class */ (function () {
         var pazzleData = data.split('').map(function (x) { return parseInt(x); });
         return pazzleData;
     };
+    Board.prototype.getRow = function (y_index) {
+        return this.dataMatrix[y_index];
+    };
+    Board.prototype.getColumn = function (x_index) {
+        return this.dataMatrix.map(function (x) { return x[x_index]; });
+    };
+    Board.prototype.getSquare = function (x_index, y_index) {
+        // xが0-2, yも0-2
+        // 0,1,2,9,10,11,18,19,20
+        // x 8, y 2
+        var x_position = Math.floor(x_index / 3);
+        var y_position = Math.floor(y_index / 3);
+        var square = new Array();
+        for (var y = 0; y < 9; y++) {
+            if (Math.floor(y / 3) == y_position) {
+                for (var x = 0; x < 9; x++) {
+                    if (Math.floor(x / 3) == x_position) {
+                        square.push(this.dataMatrix[y][x]);
+                    }
+                }
+            }
+        }
+        return square;
+    };
+    Board.prototype.printData = function () {
+        var dataMatrix = this.transferMatrix(this.data);
+        console.log(dataMatrix);
+    };
+    Board.prototype.checkCurrentBox = function (x_index, y_index) {
+        //position: 0 < x_index< 8, 0 < y_index< 8
+        // 数独での直感的な向きにして出力
+        var position = x_index + y_index * 9;
+        console.log('Index[' + x_index + ':' + y_index + '] Number is ' + this.data[position]);
+    };
     return Board;
 }());
 var b = new Board('data', 1);
 b.printData();
+b.checkCurrentBox(8, 2);
+console.log(b.getRow(1));
+console.log(b.getColumn(1));
+console.log(b.getSquare(8, 0));
+console.log(b.getSquare(0, 0));
--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,39 +1,13 @@
-let board: number[][] = new Array();
-printBoard(board)
-for (let i = 0; i < 9; i++) {
-    board[i] = new Array();
-    for (let j=0; j < 9; j++) {
-        board[i][j] = i*9 + (j+1)
-    }
-}
-console.log('default')
-printBoard(board);
-
-console.log(board[0])
-
 import testData from './data/test.json'
 
-
-//method
-function printBoard(board: number[][]) {
-    console.log(board);
-}
-
-function importPazzle(key: string , id: number){
-    // get nubmerlist from jsonfile
-    console.log(key, id)
-    console.log(testData.data[id])
-    const data: string = testData.data[id]
-    const pazzleData: number[] = data.split('').map(x => parseInt(x))
-    return pazzleData
-}
 
 class Board {
     //propaty
     data: number[]
-
+    dataMatrix: number[][]
     constructor(private key: string, private id: number) {
         this.data = this.importData(this.key, this.id) 
+        this.dataMatrix = this.transferMatrix(this.data)
     }
 
     private transferMatrix(array: number[]): number[][] {
@@ -49,12 +23,6 @@ class Board {
         return dataMatrix
     }
 
-    printData() {
-        const dataMatrix = this.transferMatrix(this.data)
-        console.log(dataMatrix)
-        
-    }
-
     private importData(key: string , id: number){
         // get nubmerlist from jsonfile
         console.log(key, id)
@@ -63,7 +31,60 @@ class Board {
         const pazzleData: number[] = data.split('').map(x => parseInt(x))
         return pazzleData
     }
+    
+    getRow(y_index: number): number[]{
+        return this.dataMatrix[y_index]
+    }
+    
+    getColumn(x_index: number): number[]{
+        return this.dataMatrix.map(x => x[x_index])
+    }
+
+    getSquare(x_index: number, y_index: number): number[]{
+        // xが0-2, yも0-2
+        // 0,1,2,9,10,11,18,19,20
+        // x 8, y 2
+        const x_position = Math.floor(x_index / 3);
+        const y_position = Math.floor(y_index / 3);
+        let square = new Array();
+        for (let y = 0; y < 9; y++) {
+            if (Math.floor(y / 3) == y_position){
+                for (let x = 0; x < 9; x++){
+                    if (Math.floor(x / 3) == x_position){
+                        square.push(this.dataMatrix[y][x])
+                    }
+                }
+            }
+        }
+        return square
+    }
+
+    SQUARE_POSITION: number[] = [
+
+    ]
+
+
+    printData() {
+        const dataMatrix= this.transferMatrix(this.data)
+        console.log(dataMatrix)     
+    }
+
+
+    checkCurrentBox(x_index: number, y_index: number) {
+        //position: 0 < x_index< 8, 0 < y_index< 8
+        // 数独での直感的な向きにして出力
+        const position: number = x_index+ y_index * 9
+        console.log('Index['+x_index+':'+y_index+'] Number is '+ this.data[position])
+        
+    
+    }
+
 }
 
 const b = new Board('data', 1)
 b.printData()
+b.checkCurrentBox(8,2)
+console.log(b.getRow(1))
+console.log(b.getColumn(1))
+console.log(b.getSquare(8,0))
+console.log(b.getSquare(0,0))
